f47ad4a3d1737326e913217742610ad7739e1a02
Rodrigo Riquelme <rodrigo.riquelme.e@gmail.com>
2019-11-14 22:24:18 -0300

    test exception handler

--- src/test/java/cl/gamelena/doggies/presentation/controller/DoggiesControllerTest.java
+++ src/test/java/cl/gamelena/doggies/presentation/controller/DoggiesControllerTest.java
@@ -1,14 +1,18 @@
 package cl.gamelena.doggies.presentation.controller;
 
+import cl.gamelena.doggies.domain.exception.DoggyNotFoundException;
 import cl.gamelena.doggies.domain.usecase.BreedsUseCase;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.client.HttpClientErrorException;
 
 import static cl.gamelena.doggies.domain.model.BreedModelStub.breedModel;
 import static org.hamcrest.Matchers.is;
@@ -30,17 +34,49 @@ class DoggiesControllerTest {
 
 
     @Test
-    void getBreed() throws Exception {
+    void getBreedWhenIsOkTheReturnsBreedModel() throws Exception {
         when(useCase.getBreed(anyString())).thenReturn(breedModel());
 
-        mockMvc.perform(
-            get("/doggies/breed/hound")
-                .accept(MimeTypeUtils.APPLICATION_JSON_VALUE)
-        )
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.breed", is(breedModel().getBreed())))
-        .andExpect(jsonPath("$.subBreeds", is(breedModel().getSubBreeds())))
-        .andExpect(jsonPath("$.images", is(breedModel().getImages())));
+        performMockMvc()
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.breed", is(breedModel().getBreed())))
+            .andExpect(jsonPath("$.subBreeds", is(breedModel().getSubBreeds())))
+            .andExpect(jsonPath("$.images", is(breedModel().getImages())));
 
+    }
+
+    @Test
+    void getBreedWhenIsDoggyNotFoundThenReturnsNotFoundStatus() throws Exception {
+        when(useCase.getBreed(anyString())).thenThrow(DoggyNotFoundException.class);
+
+        performMockMvc()
+            .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    void getBreedWhenErrorOccursThenReturnsServerErrorStatus() throws Exception {
+        when(useCase.getBreed(anyString())).thenThrow(RuntimeException.class);
+
+        performMockMvc()
+            .andExpect(status().is5xxServerError());
+    }
+
+    @Test
+    void getBreedWhenHttpClientErrorOccursThenThrowsException() throws Exception {
+
+        when(useCase.getBreed(anyString())).thenThrow(
+            new HttpClientErrorException(HttpStatus.SERVICE_UNAVAILABLE)
+        );
+
+        performMockMvc()
+                .andExpect(status().isServiceUnavailable());
+    }
+
+    private ResultActions performMockMvc() throws Exception {
+        return mockMvc.perform(
+            get("/doggies/breed/hound")
+            .accept(MimeTypeUtils.APPLICATION_JSON_VALUE)
+        );
     }
 }
